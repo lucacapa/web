299fde03909493d994aea4647bcfa4be59396c53
lucacapa <lucafilomenovicio01@gmail.com>
2025-04-29 15:50:25 +0200

    e

---
+++ javascript/03_Condicionales/act1.js
@@ -0,0 +1,9 @@
+var guardar = prompt('Tengo agujas pero no sé coser, tengo números pero no sé leer, las horas te doy, ¿Sabes quién soy?')
+
+function skibiditoilet(){ 
+     print('Skibidi Toilet')
+
+
+}
+
+skibiditoilet()
