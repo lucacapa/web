86be981d241568635b1217de69c671c481bd6a8e
lucacapa <lucafilomenovicio01@gmail.com>
2025-05-19 17:35:54 +0200

    eee

---
+++ javascript/calculadora/calculadora.js
@@ -0,0 +1,44 @@
+function number1() {
+    const display = document.querySelector('.calculator3');
+    display.textContent += '1';
+  }
+  function number2() {
+    const display = document.querySelector('.calculator3');
+    display.textContent += '2';
+  }
+  function number3() {
+    const display = document.querySelector('.calculator3');
+    display.textContent += '3';
+  }
+  function number4() {
+    const display = document.querySelector('.calculator3');
+    display.textContent += '4';
+  }
+  function number5() {
+    const display = document.querySelector('.calculator3');
+    display.textContent += '5';
+  }
+  function number6() {
+    const display = document.querySelector('.calculator3');
+    display.textContent += '6';
+  }
+  function number7() {
+    const display = document.querySelector('.calculator3');
+    display.textContent += '7';
+  }
+  function number8() {
+    const display = document.querySelector('.calculator3');
+    display.textContent += '8';
+  }
+  function number9() {
+    const display = document.querySelector('.calculator3');
+    display.textContent += '9';
+  }
+  function number0() {
+    const display = document.querySelector('.calculator3');
+    display.textContent += '0';
+  }
+  function deletee() {
+  const display = document.querySelector('.calculator3');
+  display.textContent = display.textContent.slice(0, -1);
+}
