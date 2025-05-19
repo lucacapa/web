8fa5bea069e7a021864818e4d9bf52a511c1873a
lucacapa <lucafilomenovicio01@gmail.com>
2025-05-19 16:40:49 +0000

    Deploying to gh-pages from @ lucacapa/web@2ae28f16b2e6f3cbc49753a704d0faeded8ca4fe 🚀

--- javascript/calculadora/calculadora.js
+++ javascript/calculadora/calculadora.js
@@ -1,44 +1,29 @@
-function number1() {
-    const display = document.querySelector('.calculator3');
-    display.textContent += '1';
-  }
-  function number2() {
-    const display = document.querySelector('.calculator3');
-    display.textContent += '2';
-  }
-  function number3() {
-    const display = document.querySelector('.calculator3');
-    display.textContent += '3';
-  }
-  function number4() {
-    const display = document.querySelector('.calculator3');
-    display.textContent += '4';
-  }
-  function number5() {
-    const display = document.querySelector('.calculator3');
-    display.textContent += '5';
-  }
-  function number6() {
-    const display = document.querySelector('.calculator3');
-    display.textContent += '6';
-  }
-  function number7() {
-    const display = document.querySelector('.calculator3');
-    display.textContent += '7';
-  }
-  function number8() {
-    const display = document.querySelector('.calculator3');
-    display.textContent += '8';
-  }
-  function number9() {
-    const display = document.querySelector('.calculator3');
-    display.textContent += '9';
-  }
-  function number0() {
-    const display = document.querySelector('.calculator3');
-    display.textContent += '0';
-  }
-  function deletee() {
+
+function appendNumber(number) {
+  const display = document.querySelector('.calculator3');
+  display.textContent += number;
+}
+
+function appendOperator(operator) {
+  const display = document.querySelector('.calculator3');
+  display.textContent += operator;
+}
+
+function deleteOne() {
   const display = document.querySelector('.calculator3');
   display.textContent = display.textContent.slice(0, -1);
+}
+
+function clearDisplay() {
+  const display = document.querySelector('.calculator3');
+  display.textContent = '';
+}
+
+function calculate() {
+  const display = document.querySelector('.calculator3');
+  try {
+    display.textContent = eval(display.textContent);
+  } catch {
+    display.textContent = 'Error';
+  }
 }
