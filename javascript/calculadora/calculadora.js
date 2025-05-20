8468d5a0998e00f48f36937b91d1b9ddab5d1c63
lucacapa <lucafilomenovicio01@gmail.com>
2025-05-20 15:26:36 +0000

    Deploying to gh-pages from @ lucacapa/web@33540c347a23bc2acadc5ed75d41ef7f031e8f1c 🚀

--- javascript/calculadora/calculadora.js
+++ javascript/calculadora/calculadora.js
@@ -1,29 +1,18 @@
+const bnum = document.querySelectorAll('.numero');
+const pantalla = document.getElementById('pantalla');
+const blimpiar = document.getElementById('C')
+  bnum.forEach(button => {
+    button.addEventListener('click', () => {
+      if (pantalla.textContent === '0') {
+        pantalla.textContent = button.textContent;
+      } else {
+        pantalla.textContent += button.textContent;
+      }
+    });
+});
 
-function appendNumber(number) {
-  const display = document.querySelector('.calculator3');
-  display.textContent += number;
-}
+blimpiar.addEventListener('click', () => {
+    pantalla.textContent = '0';
+});
 
-function appendOperator(operator) {
-  const display = document.querySelector('.calculator3');
-  display.textContent += operator;
-}
 
-function deleteOne() {
-  const display = document.querySelector('.calculator3');
-  display.textContent = display.textContent.slice(0, -1);
-}
-
-function clearDisplay() {
-  const display = document.querySelector('.calculator3');
-  display.textContent = '';
-}
-
-function calculate() {
-  const display = document.querySelector('.calculator3');
-  try {
-    display.textContent = eval(display.textContent);
-  } catch {
-    display.textContent = 'Error';
-  }
-}
